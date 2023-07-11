06ba340ca173533916859253c2ea8f7427f08860
Rohit Kumar <kumar1rohit@outlook.com>
2023-07-11 16:43:35 +0530

    fix db ssl error

--- helpers/database.js
+++ helpers/database.js
@@ -2,6 +2,21 @@ import { Sequelize } from 'sequelize'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const sequelize = new Sequelize(process.env.QUIZ_APP_DB_URL)
+const sequelize = new Sequelize(
+	process.env.DATABASE_NAME,
+	process.env.DATABASE_USERNAME,
+	process.env.DATABASE_PASSWORD,
+	{
+		dialect: 'postgres',
+		host: process.env.DATABASE_HOST,
+		port: process.env.DATABASE_PORT,
+		dialectOptions: {
+			ssl: {
+				require: true,
+				rejectUnauthorized: false // Set to false if the SSL/TLS certificate is self-signed or not trusted
+			}
+		}
+	}
+)
 
 export default sequelize
