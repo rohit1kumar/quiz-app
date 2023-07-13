172bd9e683161cf57544bd95725125287b24d0bb
Rohit Kumar <kumar1rohit@outlook.com>
2023-07-13 12:59:21 +0530

    fix: db ssl error for prod

--- helpers/database.js
+++ helpers/database.js
@@ -1,22 +1,36 @@
 import { Sequelize } from 'sequelize'
 import dotenv from 'dotenv'
+
 dotenv.config()
 
+const {
+	DATABASE_NAME,
+	DATABASE_USERNAME,
+	DATABASE_PASSWORD,
+	DATABASE_HOST,
+	DATABASE_PORT,
+	NODE_ENV
+} = process.env
+
+let dialectOptions = {}
+if (NODE_ENV !== 'development') {
+	dialectOptions = {
+		ssl: {
+			require: true,
+			rejectUnauthorized: false
+		}
+	}
+}
+
 const sequelize = new Sequelize(
-	process.env.DATABASE_NAME,
-	process.env.DATABASE_USERNAME,
-	process.env.DATABASE_PASSWORD,
+	DATABASE_NAME,
+	DATABASE_USERNAME,
+	DATABASE_PASSWORD,
 	{
 		dialect: 'postgres',
-		host: process.env.DATABASE_HOST,
-		port: process.env.DATABASE_PORT,
-		logging: false
-		// dialectOptions: {
-		// 	ssl: {
-		// 		// require: true,
-		// 		// rejectUnauthorized: false // Set to false if the SSL/TLS certificate is self-signed or not trusted
-		// 	}
-		// }
+		host: DATABASE_HOST,
+		port: DATABASE_PORT,
+		dialectOptions
 	}
 )
 
